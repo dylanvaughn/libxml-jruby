297993387be40104d9609d98137079c3722959c6
Michael Guterl <mguterl@gmail.com>
2008-10-01 12:14:48 -0400

    fixed assertions, they were using assert instead of assert_equal

--- test/tc_ns.rb
+++ test/tc_ns.rb
@@ -4,14 +4,14 @@ class TestNS < Test::Unit::TestCase
   def test_ns
     node = XML::Node.new('foo')
     ns = XML::NS.new(node, 'http://www.mynamespace.com', 'my_namepace')
-    assert(ns.prefix, 'my_namepace')
-    assert(ns.href, 'http://www.mynamespace.com')
+    assert_equal('my_namepace', ns.prefix)
+    assert_equal('http://www.mynamespace.com', ns.href)
   end
   
   def test_default_ns
     node = XML::Node.new('foo')
     ns = XML::NS.new(node, 'http://www.mynamespace.com', nil)
-    assert(ns.prefix, nil)
-    assert(ns.href, 'http://www.mynamespace.com')
+    assert_equal(nil, ns.prefix)
+    assert_equal('http://www.mynamespace.com', ns.href)
   end
 end
