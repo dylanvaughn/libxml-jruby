0d27fe4e29457667fd1064df091de5cd9c0ab971
Michael Guterl <mguterl@gmail.com>
2008-10-02 17:23:03 -0400

    created 100% passing test suite for verfication

---
+++ test/ts_working.rb
@@ -0,0 +1,31 @@
+# require 'tc_well_formed' FIXME Parser#context not implemented
+require 'tc_attributes'
+require 'tc_document'
+# require 'tc_document_write' FIXME Document#write not implemented
+# require 'tc_dtd' FIXME Issues with reading a Schema from a string
+# require 'tc_html_parser' FIXME HTMLParser not implemented
+require 'tc_node'
+require 'tc_node_attr'
+# require 'tc_node_cdata' FIXME Node#new_cdata
+# require 'tc_node_comment' FIXME Node#new_comment
+require 'tc_node_copy'
+# require 'tc_node_edit' FIXME Not implemented
+require 'tc_node_set'
+require 'tc_node_set2'
+# require 'tc_node_text' FIXME Node#new_text
+# require 'tc_node_xlink' FIXME Not implemented
+require 'tc_ns'
+require 'tc_parser'
+# require 'tc_parser_context' FIXME Not implemented
+# require 'tc_reader' FIXME Not implemented
+# require 'tc_relaxng' FIXME Not implemented
+# require 'tc_sax_parser' FIXME show stopping error
+# require 'tc_schema' FIXME treat string like file?
+require 'tc_traversal'
+# require 'tc_xinclude' FIXME Not implemented
+# require 'tc_xpath' FIXME this is partially implemented
+# require 'tc_xpointer' FIXME Not implemented
+
+# Compatibility
+require 'tc_properties'
+require 'tc_deprecated_require'
